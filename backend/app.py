6d555cd76fd568d92dadc1a15105063f86c70e60
nguyennguyentruong <nguyenntk.18@grad.uit.edu.vn>
2024-10-01 01:01:36 +0700

    feat: update training for phobert

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from train import train_job_embed
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/ai-core/train")
+def train():
+    train_job_embed()
+    
+    return "<p>Training</p>"
